54d1db3d39b83f731ef88069392cea427b29e57b
OlexiyKhokhlov <alex.forth@gmail.com>
2021-07-15 11:20:20 +0300

    Improve tests

--- avltree_test.go
+++ avltree_test.go
@@ -16,7 +16,13 @@ func abs(x int) int {
 const MIN = -1000
 const MAX = 1000
 
-func TestCreateAVLTree(t *testing.T) {
+func fillTree(tree *AVLTree) {
+	for i := MIN; i <= MAX; i++ {
+		tree.Insert(i, nil)
+	}
+}
+
+func TestCreate(t *testing.T) {
 	require := require.New(t)
 
 	empty_tree := NewAVLTree(func(a interface{}, b interface{}) int {
@@ -36,57 +42,7 @@ func TestCreateAVLTree(t *testing.T) {
 	require.True(empty_tree.Empty())
 }
 
-func eraseAVLTree(tree *AVLTree, t *testing.T) {
-	require := require.New(t)
-
-	var size uint = tree.Size()
-	for i := MIN; i <= MAX; i++ {
-		require.True(tree.Contains(i))
-		require.Nil(tree.Erase(i))
-		require.False(tree.Contains(i))
-		require.Error(tree.Erase(i))
-
-		size--
-		require.Equal(tree.Size(), size)
-
-		tree.CheckHeight(func(hl int, hr int) {
-			require.LessOrEqual(abs(hl-hr), 1)
-		})
-	}
-}
-
-func containsAVLTRee(tree *AVLTree, t *testing.T) {
-	require := require.New(t)
-	require.False(tree.Contains(MIN - 1))
-	require.False(tree.Contains(MAX + 1))
-	require.False(tree.Contains(MIN * 2))
-	require.False(tree.Contains(MAX * 2))
-
-	for i := MIN; i <= MAX; i++ {
-		require.True(tree.Contains(i))
-	}
-
-}
-
-func insertAVLTree(tree *AVLTree, t *testing.T) {
-	require := require.New(t)
-
-	var size uint = 0
-	for i := MIN; i <= MAX; i++ {
-		require.False(tree.Contains(i))
-		require.Nil(tree.Insert(i, 0))
-		require.Error(tree.Insert(i, 0))
-		require.True(tree.Contains(i))
-		size++
-		require.Equal(tree.Size(), size)
-
-		tree.CheckHeight(func(hl int, hr int) {
-			require.LessOrEqual(abs(hl-hr), 1)
-		})
-	}
-}
-
-func TestInsertErase(t *testing.T) {
+func TestInsert(t *testing.T) {
 	require := require.New(t)
 	require.Less(MIN, MAX)
 
@@ -103,7 +59,79 @@ func TestInsertErase(t *testing.T) {
 		return 1
 	})
 
-	insertAVLTree(tree, t)
-	containsAVLTRee(tree, t)
-	eraseAVLTree(tree, t)
+	var size uint = 0
+	for i := MIN; i <= MAX; i++ {
+		require.False(tree.Contains(i))
+		require.Nil(tree.Insert(i, 0))
+		require.Error(tree.Insert(i, 0))
+		require.True(tree.Contains(i))
+		size++
+		require.Equal(tree.Size(), size)
+
+		tree.CheckHeight(func(hl int, hr int) {
+			require.LessOrEqual(abs(hl-hr), 1)
+		})
+	}
+}
+
+func TestContains(t *testing.T) {
+	require := require.New(t)
+
+	tree := NewAVLTree(func(a interface{}, b interface{}) int {
+		first := a.(int)
+		second := b.(int)
+
+		if first == second {
+			return 0
+		}
+		if first < second {
+			return -1
+		}
+		return 1
+	})
+
+	fillTree(tree)
+
+	require.False(tree.Contains(MIN - 1))
+	require.False(tree.Contains(MAX + 1))
+	require.False(tree.Contains(MIN * 2))
+	require.False(tree.Contains(MAX * 2))
+
+	for i := MIN; i <= MAX; i++ {
+		require.True(tree.Contains(i))
+	}
+}
+
+func TestErase(t *testing.T) {
+	require := require.New(t)
+
+	tree := NewAVLTree(func(a interface{}, b interface{}) int {
+		first := a.(int)
+		second := b.(int)
+
+		if first == second {
+			return 0
+		}
+		if first < second {
+			return -1
+		}
+		return 1
+	})
+
+	fillTree(tree)
+
+	var size uint = tree.Size()
+	for i := MIN; i <= MAX; i++ {
+		require.True(tree.Contains(i))
+		require.Nil(tree.Erase(i))
+		require.False(tree.Contains(i))
+		require.Error(tree.Erase(i))
+
+		size--
+		require.Equal(tree.Size(), size)
+
+		tree.CheckHeight(func(hl int, hr int) {
+			require.LessOrEqual(abs(hl-hr), 1)
+		})
+	}
 }
